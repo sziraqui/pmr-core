0ced86b7f4ac8cfd405a858c0151c35a531abea5
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-07-13 19:39:24 +0530

    loading tfjs frozen model

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,16 @@ import * as path from 'path';
 export namespace Utils {
     export const ROOT_DIR = path.resolve(path.join(__dirname, '..'));
     export const MODEL_DIR = path.join(ROOT_DIR, 'models');
+    const FILE_PREFIX = 'file:://';
 
     export async function loadKerasModel(modelName: string) {
-        return await tf.loadLayersModel(path.join(MODEL_DIR, modelName, 'model.json'));
+        return await tf.loadLayersModel(path.join(FILE_PREFIX + MODEL_DIR, modelName, 'model.json'));
     }
+
+    export async function loadFrozenModel(modelName: string) {
+        const modelDef = FILE_PREFIX + path.join(MODEL_DIR, modelName, 'tensorflowjs_model.pb');
+        const weights = FILE_PREFIX + path.join(MODEL_DIR, modelName, 'weights_manifest.json');
+        return await tf.loadFrozenModel(modelDef, weights);
+    }
+
 }
