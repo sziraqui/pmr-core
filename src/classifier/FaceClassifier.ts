c93fd0037f020f263a2a2b78999217b1fb01694a
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-08-08 20:42:13 +0530

    fix undefined label issue

--- src/classifier/FaceClassifier.ts
+++ src/classifier/FaceClassifier.ts
@@ -1,23 +1,22 @@
-import { create as CreateClassifier, KNNClassifier } from '@tensorflow-models/knn-classifier';
 import { readFileSync, existsSync } from 'fs';
+import * as tf from '@tensorflow/tfjs-core';
 import { utils } from '..';
 import * as path from 'path';
-import * as tf from '@tensorflow/tfjs-node';
 import * as _ from 'lodash';
-
-export const MAX_CLASSIFICATIONS = 10;
+import * as knn from '../../../knn-classifier/src/index';
+export const K = 4;
 
 export class FaceClassifier {
     private static instance: FaceClassifier;
-    private classifier: KNNClassifier;
+    private classifier;
     private labels: {};
     private constructor() {
-        this.classifier = CreateClassifier();
+        this.classifier = knn.create();
     }
     static async getInstance() {
         if (!FaceClassifier.instance) {
             FaceClassifier.instance = new FaceClassifier();
-            await FaceClassifier.instance.load('knn_classifier');
+            await FaceClassifier.instance.load('knn_classifier_10examples');
         }
         return FaceClassifier.instance;
     }
@@ -32,7 +31,7 @@ export class FaceClassifier {
     }
 
     async load(modelName: string) {
-        return this.loadWeights(path.join(utils.MODEL_DIR, modelName))
+        return this.loadWeights(path.join(utils.MODEL_DIR, modelName, 'model.json'))
             .then(weights => weights["data"])
             .then(dataList => this.initModel(dataList))
             .then(success => {
@@ -46,20 +45,21 @@ export class FaceClassifier {
         let labels = {};
         let clasNum = 0;
         for (let data of dataList) {
-            if (labels[data.personname] != undefined) {
+            if (labels[data.personname] === undefined) {
                 labels[data.personname] = clasNum;
                 clasNum++;
             }
         }
-        await dataList.forEach((data) => {
-            this.classifier.addExample(tf.tensor(Float64Array.from(data.embedding)), labels[data.personname]);
-        });
         this.labels = _.invert(labels);
+        await dataList.forEach((data) => {
+            let tsr = tf.tensor1d(data.embedding);
+            this.classifier.addExample(tsr, data.personname);
+        });
         return await true;
     }
 
     async predict(embedding: Float64Array) {
-        let res = await this.classifier.predictClass(tf.tensor(embedding), MAX_CLASSIFICATIONS);
+        let res = await this.classifier.predictClass(tf.tensor1d(Array.from(embedding)), K);
         return res;
     }
 
